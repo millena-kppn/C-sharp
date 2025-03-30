0157a39904870e35c60cf259bae54ed511a586df
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2025-03-30 19:14:29 -0300

    Guardando informações com ReadLine

---
+++ Praticas/Guardando-Dados/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guardando_Dados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome:");
+            string seuNome = Console.ReadLine();
+            Console.Write("Digite sua idade:");
+            int suaIdade = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Bem vindo(a) {seuNome}! Você tem {suaIdade} anos.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Praticas/Guardando-Dados/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Guardando-Dados")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Guardando-Dados")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("618fed99-24b0-46f7-bf5d-bf5731637381")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
